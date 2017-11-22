8fb74631bf054c2392347cac6db4fc71e65759c9
Prime-Host <knordloh@nordloh-webdesign.de>
2017-11-22 16:00:40 +0100

    automatic setup and new README

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ First make sure you have the newest version of all images.
 
 
 ```bash
-docker pull nginx
-docker pull jwilder/docker-gen
-docker pull jrcs/letsencrypt-nginx-proxy-companion
 ```
 
 Clone the Repo and start all containers.
 
 ```bash
-MY_EXTERNAL_IP=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
+git clone https://github.com/Prime-Host/letsenrypt-nginx-proxy.git
+./run.sh
 ```
--- run.sh
+++ run.sh
@@ -19,7 +19,13 @@ docker network create $NETWORK
 # 3. Download the latest version of nginx.tmpl
 curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > nginx.tmpl
 
-# 4. Start proxy
+# 4 set external IP in .env file
+MY_EXTERNAL_IP=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
+
+# 5 download new container versions
+docker-compose pull
+
+# 6. Start proxy
 docker-compose up -d
 
 exit 0
