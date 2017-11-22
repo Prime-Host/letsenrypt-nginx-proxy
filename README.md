93b56a2fa28ebffe4921303716eb6433d805d10d
Prime-Host <knordloh@nordloh-webdesign.de>
2017-11-22 16:10:27 +0100

    add env file

--- README.md
+++ README.md
@@ -3,12 +3,10 @@
 First make sure you have the newest version of all images.
 
 
-```bash
-```
-
 Clone the Repo and start all containers.
 
 ```bash
 git clone https://github.com/Prime-Host/letsenrypt-nginx-proxy.git
+cd letsenrypt-nginx-proxy
 ./run.sh
 ```
--- run.sh
+++ run.sh
@@ -5,13 +5,8 @@
 # Source: https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion
 #
 
-# 1. Check if .env file exists
-if [ -e .env ]; then
-    source .env
-else 
-    echo "Please set up your .env file before starting your enviornment."
-    exit 1
-fi
+# 1. mv sample to .env
+mv .env.sample .env
 
 # 2. Create docker network
 docker network create $NETWORK
